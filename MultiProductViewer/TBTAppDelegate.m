7242fa9bd8364f0c05ac311ba01f8d100ab82489
Jack Nutting <jacknutting@mac.com>
2014-03-21 14:15:13 +0100

    changed copyright

--- MultiProductViewer/TBTAppDelegate.m
+++ MultiProductViewer/TBTAppDelegate.m
@@ -3,7 +3,7 @@
 //  MultiProductViewer
 //
 //  Created by JN on 2014-3-18.
-//  Copyright (c) 2014 Rebisoft. All rights reserved.
+//  Copyright (c) 2014 thoughtbot, inc.. All rights reserved.
 //
 
 #import "TBTAppDelegate.h"
